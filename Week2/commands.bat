252839bbd37aaca8faa1f391726b62800e5b8910
Nicholas Spagnola <39714515+EvaStanAccount@users.noreply.github.com>
2024-01-30 10:55:34 -0500

    Update commands.bat

--- Week2/commands.bat
+++ Week2/commands.bat
@@ -2,3 +2,5 @@ echo "[+] Compiling malicious.cs
 C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe /out:C:\Users\Administrator\malicious.exe /platform:x64 C:\Users\administrator\Downloads\malicious.cs
 
 echo "[*] Running malicious.exe"
+
+C:\Users\Administrator\malicious.exe
