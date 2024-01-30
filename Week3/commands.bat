d14d5b9aa8896527fd338e516a98384ea8fe553f
Nicholas Spagnola <39714515+EvaStanAccount@users.noreply.github.com>
2024-01-30 11:00:43 -0500

    Update commands.bat

--- Week3/commands.bat
+++ Week3/commands.bat
@@ -1,8 +1,8 @@
 echo "[+] Extracting sysmon"
-set /p "hit enter to continue..."
+set /p a="hit enter to continue..."
 powershell Expand-Archive -Path C:\Users\%USERNAME%\Downloads\Sysmon.zip
 echo "[*] Sysmon extracted"
-set /p "hit enter to continue to install Sysmon..."
+set /p b="hit enter to continue to install Sysmon..."
 C:\Users\%USERNAME%\Downloads\Sysmon\sysmon.exe -i C:\Users\%USERNAME%\Downloads\sysmon_config.xml
-set /p "hit enter to reconfigure sysmon..."
+set /p c="hit enter to reconfigure sysmon..."
 C:\Users\%USERNAME%\Downloads\Sysmon\sysmon.exe -c C:\Users\%USERNAME%\Downloads\sysmon_config_mali.xml
