25cc66c2367dea1dce6ccd34c29947da210bc25b
Nick S <nickspags34@gmail.com>
2024-02-07 11:35:40 -0500

    week5 update 2

--- Week5/week5-commands.bat
+++ Week5/week5-commands.bat
@@ -8,10 +8,10 @@ logman query providers Microsoft-Windows-DotNETRuntime
 set /p c="hit enter to continue..."
 logman create trace it4077-trace -ets
 set /p d="hit enter to continue..."
-logman update it4077-trace -p Microsoft-Windows-DotNETRuntime 0x18
+logman update it4077-trace -p Microsoft-Windows-DotNETRuntime 0x18 -ets
 set /p e="hit enter to continue..."
 logman query it4077-trace -ets
 set /p f="hit enter to continue..."
 C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe /out:C:\Users\%USERNAME%\ETWCanSeeAssemblyNames.exe /platform:x64 C:\Users\%USERNAME%\Downloads\MsgBox.cs
 C:\Users\%USERNAME%\ETWCanSeeAssemblyNames.exe
-
+logman stop it4077-trace -ets
