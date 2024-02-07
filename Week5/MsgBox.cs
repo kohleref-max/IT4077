643af22c93a09d9aed8556871797e616b604baef
Nick S <nickspags34@gmail.com>
2024-02-07 11:29:39 -0500

    week5

---
+++ Week5/MsgBox.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Window.Forms;
+
+namespace IT4077
+{
+	public class DangerousClass
+	{
+		public static void Main(string[] args)
+		{
+			ETWcanSeeClassNamesandLoads();
+		}
+		private static void ETWcanSeeClassNamesandLoads
+		{
+			MessageBox.Show("Executing","Code");
+		}
+	}
+}
---
+++ Week5/week5-commands.bat
@@ -0,0 +1,16 @@
+echo "[+] Starting Week5 Assignment"
+echo "[+] Hit Enter to begin..."
+set /p a="hit enter to continue..."
+logman query providers
+set /p b="hit enter to continue..."
+logman query providers Microsoft-Windows-DotNETRuntime
+set /p c="hit enter to continue..."
+logman create trace it4077-trace -ets
+set /p d="hit enter to continue..."
+logman update it4077-trace -p Microsoft-Windows-DotNETRuntime 0x18
+set /p e="hit enter to continue..."
+logman query it4077-trace -ets
+set /p f="hit enter to continue..."
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe /out:C:\Users\%USERNAME%\ETWCanSeeAssemblyNames.exe /platform:x64 C:\Users\%USERNAME%\Downloads\MsgBox.cs
+C:\Users\%USERNAME%\ETWCanSeeAssemblyNames.exe
+
