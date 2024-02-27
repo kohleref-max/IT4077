d3a6cd35d7e63931b16ea4999d3a652c62c8ff1a
Nicholas Spagnola <39714515+EvaStanAccount@users.noreply.github.com>
2024-02-27 11:55:57 -0500

    Add files via upload

---
+++ Week8/SysmonConfigure.ps1
@@ -0,0 +1,3 @@
+﻿$wc = New-Object Net.WebClient
+$wc.DownloadFile("https://raw.githubusercontent.com/EvaStanAccount/IT4077/Main/Week8/sysmonconfig-week8.xml","C:\Users\Administrator\sysmonconfig-week8.xml")
+C:\Users\Administrator\Downloads\Sysmon\Sysmon.exe -c C:\Users\Administrator\sysmonconfig-week8.xml
